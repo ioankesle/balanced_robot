84b2ffc6270a249f06fc2be3a21c873ede69af39
Ioannis Petridis <petridisioan@gmail.com>
2018-10-01 15:25:20 +0200

    Update bot.cpp

--- balance_bot_rpi_part/bot.cpp
+++ balance_bot_rpi_part/bot.cpp
@@ -18,6 +18,7 @@
 #include <semaphore.h>
 #include <sched.h>
 #include <poll.h>
+#include <sys/timerfd.h>
 
 #include "demo_dmp.h"
 
@@ -49,9 +50,9 @@ float angleNow = 0.0;
 
 
 // PID IMU coeff
-float kp = 13.5; //5
+float kp = 12.0; //5
 float ki = 0.0;
-float kd = 1.2;
+float kd = 0.0; //19
 int pidOut_IMU = 0;
 
 // PID VEL coeff
@@ -70,7 +71,7 @@ int* joyS = {&p};
 
 
 // arduino
-Pi2c arduino(4); //Create a new object "arduino" using address "0x04"
+Pi2c arduino(9); //Create a new object "arduino" using address "0x09"
 int16_t r = 0;
 int16_t* receive  = {&r};
 
@@ -81,14 +82,11 @@ int16_t* receive  = {&r};
 pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;
 
+sem_t sec_mutex, thrd_mutex;
+
 int fl = 0;
 int count = 0;
 //int flag= 0;
-
-long poll_thread1_period = 6000L; // poll speed every 10ms  IMU
-long poll_thread2_period = 6000L; // poll speed every 20ms  bluetooth
-long poll_thread3_period = 6000L; // poll speed every 10ms  PID pitch
-long poll_thread4_period = 10000L; // poll speed every 5ms  PID velocity
 
 int* getJoystickState(char data[8])
 {
@@ -98,7 +96,7 @@ int* getJoystickState(char data[8])
     int joyY = (data[4] - 48) * 100 + (data[5] - 48) * 10 + (data[6] - 48);
     joyX = joyX - 200; // Offset to avoid
     joyY = joyY - 200; // transmitting negative numbers
-    //printf("joyX: %d	joyY: %d \n", joyX, joyY);   // <--------------------------------
+    //printf("joyX: %d	joyY: %d \n", joyX, joyY);
     //printf("received %d, %d\n", data[1], data[2]);
     Joy[0] = joyX;
     Joy[1] = joyY;
@@ -174,62 +172,107 @@ int mymillis()
     return (unsigned long long)(tv.tv_sec) * 1000 + (unsigned long long)(tv.tv_usec) / 1000;
 }
 
-void* function1(void* period)
+struct periodic_info
+{
+	int timer_fd;
+	unsigned long long wakeups_missed;
+};
+
+static int make_periodic (unsigned int period, struct periodic_info *info)
+{
+	int ret;
+	unsigned int ns;
+	unsigned int sec;
+	int fd;
+	struct itimerspec itval;
+
+	/* Create the timer */
+	//fd = timerfd_create (CLOCK_REALTIME, 0);
+	fd = timerfd_create (CLOCK_MONOTONIC, 0);
+	info->wakeups_missed = 0;
+	info->timer_fd = fd;
+	if (fd == -1)
+		return fd;
+
+	/* Make the timer periodic */
+	sec = period/1000000;
+	ns = (period - (sec * 1000000)) * 1000;
+	itval.it_interval.tv_sec = sec;
+	itval.it_interval.tv_nsec = ns;
+	itval.it_value.tv_sec = sec;
+	itval.it_value.tv_nsec = ns;
+	ret = timerfd_settime (fd, 0, &itval, NULL);
+	return ret;
+}
+
+static void wait_period (struct periodic_info *info)
+{
+	unsigned long long missed;
+	int ret;
+
+	/* Wait for the next timer event. If we have missed any the
+	   number is written to "missed" */
+	ret = read (info->timer_fd, &missed, sizeof (missed));
+	if (ret == -1)
+	{
+		perror ("read timer");
+		return;
+	}
+
+	/* "missed" should always be >= 1, but just to be sure, check it is not 0 anyway */
+	if (missed > 0)
+		info->wakeups_missed += (missed - 1);
+}
+
+void* function1(void* arg)
 {
 
-    struct timespec timeoutStart, timeoutEnd;
-    double mainTdiff = 0.0;
-    long ps = *((long*)period);
-	float pitch;
+    //  struct timespec timeoutStart, timeoutEnd;
+    //  double mainTdiff = 0.0;
+		
+	struct periodic_info info;
+	make_periodic (10000, &info);
 
+	float pitch;
 	
     while (fl == 0) {
-        // sem_wait(&sec_mutex);
-        clock_gettime(CLOCK_REALTIME, &timeoutStart);
+        sem_wait(&sec_mutex);
 
-        // Lock mutex and then wait for signal to relase mutex
-        pthread_mutex_lock(&count_mutex);
-
-		loop();
+    //  clock_gettime(CLOCK_MONOTONIC, &timeoutStart);
+	loop();
         //yaw = get_yaw();
         pitch = get_pitch();
         //roll = get_roll();
-		angleNow = pitch;
+	angleNow = pitch;
         //printf("pitch  %7.2f %7.2f %7.2f \n", yaw, pitch, roll);
-		//printf("angleNow %7.2f \n", angleNow);
+	//printf("angleNow %7.2f \n", angleNow);
 
-        pthread_mutex_unlock(&count_mutex);
+        //pthread_mutex_unlock(&count_mutex);
+	sem_post(&sec_mutex);
+	wait_period (&info);
 
-        clock_gettime(CLOCK_REALTIME, &timeoutEnd);
-        mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9; //sec
-        long diff_us = mainTdiff * 1000000L; //us
-        long sleep_us = ps - diff_us; //- 165L;
+    //  clock_gettime(CLOCK_MONOTONIC, &timeoutEnd);
+    //  mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9; //sec
+    //  long diff_us = mainTdiff * 1000000L; //us
 
-        if (sleep_us > 0) {
-            timespec_add_us(&timeoutEnd, sleep_us);
-            clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &timeoutEnd, NULL);
-        }
-        clock_gettime(CLOCK_REALTIME, &timeoutEnd);
-        mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9;
-        //long time_elapsed = mainTdiff * 1000000L; //us
-
-        //printf( "Diff, Sleep, Elasped Time1: %lu, %lu, %lu \n", diff_us, sleep_us, time_elapsed);
+    //  printf( "Elasped Time1: %lu \n", diff_us);
     }
     return NULL;
 }
 
-void* function2(void* period)
+void* function2(void* arg)
 {
-    struct timespec timeoutStart, timeoutEnd;
-    double mainTdiff = 0.0;
-    long ps = *((long*)period);
+    //  struct timespec timeoutStart, timeoutEnd;
+    //  double mainTdiff = 0.0;
+
+	struct periodic_info info;
+	make_periodic (10000, &info);
+	
 	joyS[0] = 0;
 
     while (fl == 0) {
-        //sem_wait(&sec_mutex);
-        clock_gettime(CLOCK_REALTIME, &timeoutStart);
 
-        pthread_mutex_lock(&count_mutex);
+    //  clock_gettime(CLOCK_MONOTONIC, &timeoutStart);
         // read data from the client (bluetooth)
         bytes_read = read(client, buf, sizeof(buf));
 
@@ -250,131 +293,129 @@ void* function2(void* period)
                 exit(0);
             }
         }
-		//printf("target1: %d	target2: %d \n", joyS[0], joyS[1]);
+	//printf("target1: %d	target2: %d \n", joyS[0], joyS[1]);
 
         //sendBlueToothData();  //send data to phone
-        pthread_mutex_unlock(&count_mutex);
+	wait_period (&info);
+        //pthread_mutex_unlock(&count_mutex);
 
-        clock_gettime(CLOCK_REALTIME, &timeoutEnd);
-        mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9;
-        long diff_us = mainTdiff * 1000000L;
-        long sleep_us = ps - diff_us; //- 165L;
-        if (sleep_us > 0) {
-            timespec_add_us(&timeoutEnd, sleep_us);
-            clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &timeoutEnd, NULL);
-        }
-        clock_gettime(CLOCK_REALTIME, &timeoutEnd);
-        mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9;
-        //long time_elapsed = mainTdiff * 1000000L; //us
-
-        //printf( "Diff, Sleep, Elasped Time2: %lu, %lu, %lu \n", diff_us, sleep_us, time_elapsed);
+    //  clock_gettime(CLOCK_MONOTONIC, &timeoutEnd);
+    //  mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9;
+    //  long diff_us = mainTdiff * 1000000L;
+	
+    //  printf( "Elasped Time2: %lu \n", diff_us);
     }
 
     return NULL;
 }
 
-void* function3(void* period)
+void* function3(void* arg)
 {
 
-    struct timespec timeoutStart, timeoutEnd;
-    double mainTdiff = 0.0;
-    long ps = *((long*)period);
+    //struct timespec timeoutStart, timeoutEnd;
+    //double mainTdiff = 0.0;
+	
+	struct periodic_info info;
+	make_periodic (1000, &info);
+	
 	float ITerm = 0.0;
 	float DTerm = 0.0;
 	int outMax = 550;
 	int outMin = -550;
 	float targetAngle = 0.0;
-	float angleZero = 1.0;
+	float angleZero = 1.4;
+	
+	float angleLast = 0;
 
 	float errAngle = 0.0;
-	float lastErrAngle = 0.0;
+	//float lastErrAngle = 0.0;
 	
 	int16_t speedTarget1 = 0;
 	int16_t speedTarget2 = 0;
 
     while (fl == 0) {
-        // sem_wait(&sec_mutex);
-        clock_gettime(CLOCK_REALTIME, &timeoutStart);
-
+		
+	//clock_gettime(CLOCK_MONOTONIC, &timeoutStart);
+        sem_wait(&sec_mutex);
         // Lock mutex and then wait for signal to relase mutex
-        pthread_mutex_lock(&count_mutex);
+        //pthread_mutex_lock(&count_mutex);
 		
-		if ( ((angleNow > 0) && (angleNow - angleZero) > 30.0) || ((angleNow < 0) &&  (angleNow + angleZero) < -30.0) ) {
+	if ( ((angleNow > 0) && (angleNow - angleZero) > 40.0) || ((angleNow < 0) &&  (angleNow + angleZero) < -40.0) ) {
 				
-				DTerm = 0.0;
-				ITerm = 0.0;
-				pidOut_IMU = 0;
-				pidOut_Speed = 0.0;
-				//printf("	pidOut_IMU_off  %d\n", pidOut_IMU );
+			DTerm = 0.0;
+			ITerm = 0.0;
+			pidOut_IMU = 0;
+			pidOut_Speed = 0.0;
+			//printf("	pidOut_IMU_off  %d\n", pidOut_IMU );
 		}
-		else{
+	else{
 			
-			targetAngle = angleZero - pidOut_Speed ;
+		targetAngle = angleZero - pidOut_Speed ;
+		errAngle = targetAngle - angleNow;				
+		ITerm += ki * errAngle;
+			
+		if(ITerm > outMax) ITerm = outMax;
+		else if(ITerm < outMin) ITerm = outMin;
+			
+		DTerm = angleNow - angleLast;
+		//DTerm = errAngle - lastErrAngle;	
+			
+		//pidOut_IMU = round(ITerm - (kp * DTerm) - (kd * DTerm));			 
+		pidOut_IMU = round((kp * errAngle) + ITerm - (kd * DTerm));			
+		if (pidOut_IMU > outMax){
+		//ITerm -= pidOut_IMU - outMax;
+		pidOut_IMU = outMax;
+		}
+		else if (pidOut_IMU < outMin){
+		//ITerm += outMin -  pidOut_IMU;
+		pidOut_IMU = outMin;
+		}
+			
+		//lastErrAngle = errAngle;
+		angleLast = angleNow;
+		//printf("	pidOut_IMU  %d\n", pidOut_IMU );
+	}
+		
+		
+	speedTarget1 = pidOut_IMU + (joyS[0]*5); // re-scale 0-100 ->  0-500
+	speedTarget2 = pidOut_IMU - (joyS[0]*5);
+		
+	if (speedTarget1 > 550) speedTarget1 = 550;
+	else if (speedTarget1 < -550) speedTarget1 = -550;
+	if (speedTarget2 > 550) speedTarget2 = 550;
+	else if (speedTarget2 < -550) speedTarget2 = -550;		
+		
+	//speedTarget1 = joyS[0]*5;
+	//speedTarget2 = joyS[0]*5;
+       
+	arduino.i2cWriteArduino2Int(speedTarget1, speedTarget2 );  // max 520
 
-			errAngle = angleNow - targetAngle;				
-			ITerm += (ki * errAngle);
-			DTerm = errAngle - lastErrAngle;	
-			//if(ITerm > outMax) ITerm = outMax;
-			//else if(ITerm < outMin) ITerm = outMin;			 
-			pidOut_IMU = round((kp * errAngle) + ITerm + (kd * DTerm));			 
-			
-			if (pidOut_IMU > outMax){
-			ITerm -= pidOut_IMU - outMax;
-			pidOut_IMU = outMax;
-			}
-			else if (pidOut_IMU < outMin){
-			ITerm += outMin -  pidOut_IMU;
-			pidOut_IMU = outMin;
-			}
-			
-			lastErrAngle = errAngle;
-			//angleLast = angleNow;
-			//printf("	pidOut_IMU  %d\n", pidOut_IMU );
-		}
-		
-		
-		speedTarget1 = pidOut_IMU + (joyS[0]*5); // re-scale 0-100 ->  0-500
-		speedTarget2 = pidOut_IMU - (joyS[0]*5);
-		
-		if (speedTarget1 > 550) speedTarget1 = 550;
-		else if (speedTarget1 < -550) speedTarget1 = -550;
-		if (speedTarget2 > 550) speedTarget2 = 550;
-		else if (speedTarget2 < -550) speedTarget2 = -550;		
-		
-        
-		arduino.i2cWriteArduino2Int(speedTarget1, speedTarget2 );  // max 520
-		//printf("target1: %d	target2: %d \n", speedTarget1, speedTarget2);
+	//printf("target1: %d	target2: %d \n", speedTarget1, speedTarget2);
 		
         //Send an 8 bit integer
         //arduino.i2cWriteArduinoInt(pidOut_IMU);
+	sem_post(&sec_mutex);
+	wait_period (&info);
+        //pthread_mutex_unlock(&count_mutex);
 
-        pthread_mutex_unlock(&count_mutex);
+    //  clock_gettime(CLOCK_MONOTONIC, &timeoutEnd);
+    //  mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9; //sec
+    //  long diff_us = mainTdiff * 1000000L; //us
 
-        clock_gettime(CLOCK_REALTIME, &timeoutEnd);
-        mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9; //sec
-        long diff_us = mainTdiff * 1000000L; //us
-        long sleep_us = ps - diff_us; //- 165L;
-
-        if (sleep_us > 0) {
-            timespec_add_us(&timeoutEnd, sleep_us);
-            clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &timeoutEnd, NULL);
-        }
-        clock_gettime(CLOCK_REALTIME, &timeoutEnd);
-        mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9;
-        //long time_elapsed = mainTdiff * 1000000L; //us
-
-        //printf( "Diff, Sleep, Elasped Time3: %lu, %lu, %lu \n", diff_us, sleep_us, time_elapsed);
+    //  printf( "Elasped Time3: %lu \n", diff_us);
 		
     }
     return NULL;
 }
 
-void* function4(void* period)
+void* function4(void* arg)
 {
 
-    struct timespec timeoutStart, timeoutEnd;
-    double mainTdiff = 0.0;
-    long ps = *((long*)period);
+    //struct timespec timeoutStart, timeoutEnd;
+    //double mainTdiff = 0.0;
+
+	struct periodic_info info;
+	make_periodic (5000, &info);
 
 	int speedNow = 0;
 	int targetSpeed = 0;
@@ -391,58 +432,48 @@ void* function4(void* period)
 
     while (fl == 0) {
         // sem_wait(&sec_mutex);
-        clock_gettime(CLOCK_REALTIME, &timeoutStart);
+        // clock_gettime(CLOCK_MONOTONIC, &timeoutStart);
 
         // Lock mutex and then wait for signal to relase mutex
-        pthread_mutex_lock(&count_mutex);
+        // pthread_mutex_lock(&count_mutex);
 		
-		//Receive from the Arduino and put the contents into the "receive" char array
+	//Receive from the Arduino and put the contents into the "receive" char array
         receive = arduino.i2cReadArduinoInt();
 		
         //Print out what the Arduino is sending...
         //printf("rcv1: %d \n", receive[0]);
 		
-		speedNow = receive[0];
-		targetSpeed = joyS[1]*5;
+	speedNow = receive[0];
+	targetSpeed = joyS[1]*5;
 		
-		//printf("target speed: %d speed now: %d \n", targetSpeed , speedNow);
+	//printf("target speed: %d speed now: %d \n", targetSpeed , speedNow);
 		                            
-		errSpeed = speedNow - targetSpeed;				
-		ITerm += (ki_v * errSpeed);
-		DTerm = errSpeed - lastErrSpeed;	
-		//if(ITerm > outMax) ITerm = outMax;
-		//else if(ITerm < outMin) ITerm = outMin;			 
-		pidOut_Speed = (kp_v * errSpeed) + ITerm + (kd_v * DTerm);
+	errSpeed = speedNow - targetSpeed;				
+	ITerm += (ki_v * errSpeed);
+	DTerm = errSpeed - lastErrSpeed;	
+	//if(ITerm > outMax) ITerm = outMax;
+	//else if(ITerm < outMin) ITerm = outMin;			 
+	pidOut_Speed = (kp_v * errSpeed) + ITerm + (kd_v * DTerm);
 		
 		
-		if (pidOut_Speed > outMax){
-			ITerm -= pidOut_Speed - outMax;
-			pidOut_Speed = outMax;
-		}
-		else if (pidOut_Speed < outMin){
-			ITerm += outMin - pidOut_Speed;
-			pidOut_Speed = outMin;
-		}
+	if (pidOut_Speed > outMax){
+		ITerm -= pidOut_Speed - outMax;
+		pidOut_Speed = outMax;
+	}
+	else if (pidOut_Speed < outMin){
+		ITerm += outMin - pidOut_Speed;
+		pidOut_Speed = outMin;
+	}
 		
-		lastErrSpeed = errSpeed;
-		//printf("	pidOut_Speed  %d\n", pidOut_Speed );
-		
-        pthread_mutex_unlock(&count_mutex);
+	lastErrSpeed = errSpeed;
+	//printf("	pidOut_Speed  %d\n", pidOut_Speed );
 
-        clock_gettime(CLOCK_REALTIME, &timeoutEnd);
-        mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9; //sec
-        long diff_us = mainTdiff * 1000000L; //us
-        long sleep_us = ps - diff_us; //- 165L;
+	wait_period (&info);
+    //    clock_gettime(CLOCK_MONOTONIC, &timeoutEnd);
+    //    mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9; //sec
+    //    long diff_us = mainTdiff * 1000000L; //us
 
-        if (sleep_us > 0) {
-            timespec_add_us(&timeoutEnd, sleep_us);
-            clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &timeoutEnd, NULL);
-        }
-        clock_gettime(CLOCK_REALTIME, &timeoutEnd);
-        mainTdiff = (timeoutEnd.tv_sec - timeoutStart.tv_sec) + (timeoutEnd.tv_nsec - timeoutStart.tv_nsec) / 1E9;
-      //  long time_elapsed = mainTdiff * 1000000L; //us
-
-        //printf( "Diff, Sleep, Elasped Time4: %lu, %lu, %lu \n", diff_us, sleep_us, time_elapsed);
+    //    printf( "Elasped Time4: %lu \n", diff_us);
     }
     return NULL;
 }
@@ -460,9 +491,9 @@ int main(int argc, char* argv[])
 
     pid_t pid = 0;
     int policy = SCHED_FIFO;
-    int maxpr = 0; // 0-99
+    //int maxpr = 0; // 0-99
     struct sched_param param;
-    maxpr = sched_get_priority_max(policy);
+    int maxpr = sched_get_priority_max(policy);
     param.sched_priority = maxpr;
     sched_setscheduler(pid, policy, &param);
 
@@ -471,7 +502,7 @@ int main(int argc, char* argv[])
     pthread_attr_t my_attr;
     pthread_attr_init(&my_attr);
 
-    //sem_init(&sec_mutex,0,1);
+    sem_init(&sec_mutex,0,1);
 
     setup(); //IMU
 
@@ -514,67 +545,70 @@ int main(int argc, char* argv[])
 
     param.sched_priority = 5;
     pthread_attr_setschedparam(&my_attr, &param);
-    chk = pthread_create(&thread1, &my_attr, &function1, &poll_thread1_period);
+    chk = pthread_create(&thread1, &my_attr, &function1, NULL);
     if (chk) {
         printf("unable to create poll thread1");
     }
-
-    param.sched_priority = 5;
+	
+    param.sched_priority = 15;
     pthread_attr_setschedparam(&my_attr, &param);
-    chk = pthread_create(&thread2, &my_attr, &function2, &poll_thread2_period);
+    chk = pthread_create(&thread2, &my_attr, &function2, NULL);
     if (chk) {
         printf("unable to create poll thread2");
     }
-
-    param.sched_priority = 5;
+	
+    param.sched_priority = 6;
     pthread_attr_setschedparam(&my_attr, &param);
-    chk = pthread_create(&thread3, &my_attr, &function3, &poll_thread3_period);
+    chk = pthread_create(&thread3, &my_attr, &function3, NULL);
     if (chk) {
         printf("unable to create poll thread3");
     }
 
-    param.sched_priority = 5;
+    param.sched_priority = 7;
     pthread_attr_setschedparam(&my_attr, &param);
-    chk = pthread_create(&thread4, &my_attr, &function4, &poll_thread4_period);
+    chk = pthread_create(&thread4, &my_attr, &function4, NULL);
     if (chk) {
         printf("unable to create poll thread4");
     }
 
-    printf("Entered \n");
+	
     do {
         printf("Enter pid params or 0 to exit\n");
         scanf("%s", input);
+		
         //printf("Entered %s\n", input);
-        if (strcmp(input, "p+") == 0) {
-            kp += 0.1;
+        if (strcmp(input, "1+") == 0) {
+            kp += 0.5;
             printf("kp = %f\n", kp);
         }
-        else if (strcmp(input, "p-") == 0) {
-            kp -= 0.1;
+        else if (strcmp(input, "1-") == 0) {
+            kp -= 0.5;
             printf("kp = %f\n", kp);
         }
 
-        if (strcmp(input, "i+") == 0) {
+        if (strcmp(input, "2+") == 0) {
             ki += 0.001;
             printf("ki = %f\n", ki);
         }
-        else if (strcmp(input, "i-") == 0) {
+        else if (strcmp(input, "2-") == 0) {
             ki -= 0.001;
             printf("ki = %f\n", ki);
         }
 
-        if (strcmp(input, "d+") == 0) {
-            kd += 0.001;
+        if (strcmp(input, "3+") == 0) {
+            kd += 0.5;
             printf("kd = %f\n", kd);
         }
-        else if (strcmp(input, "d-") == 0) {
-            kd -= 0.001;
+        else if (strcmp(input, "3-") == 0) {
+            kd -= 0.5;
             printf("kd = %f\n", kd);
         }
 		printf( "pidOut_IMU: %d \n", pidOut_IMU);
 
     } while (0 != strcmp(input, "0")); // key 0
     fl = 1;
+	
+    arduino.i2cWriteArduino2Int(0,0); 
 
     // close connection
     close(client);
@@ -584,13 +618,13 @@ int main(int argc, char* argv[])
     pthread_join(thread2, NULL);
     pthread_join(thread3, NULL);
     pthread_join(thread4, NULL);
-
+	
+	sem_destroy(&sec_mutex);
     //sem_getvalue(&sec_mutex, &value);
     //printf("The value of the semaphors is %d\n", value);
-
-    printf("EXITINGGGGG\n");
     pthread_attr_destroy(&my_attr);
-
+	
+    printf("EXITINGGGGG\n");
     exit(EXIT_SUCCESS);
     return 0;
 }
